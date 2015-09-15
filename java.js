d7c524526981b0d7cd899ed4f0957cd5473d0541
Addison Chen <addison.k.chen@gmail.com>
2015-09-15 00:19:57 -0700

    added scroll nav and summary section

--- java.js
+++ java.js
@@ -13,62 +13,63 @@ $(document).ready(function(){
           navigationBar.css('background-color','rgba(0,0,0,0)');
           triangle.css('display','none');
         }
+
+        $('.target').each(function() {
+            if(scroll_pos + 53 >= $(this).position().top) {
+                var id = $(this).attr('id');
+                $('.navigationBar a').removeClass('active');
+                $('.navigationBar a[href=#'+ id +']').addClass('active');
+            } else if (scroll_pos <= 500){
+                $('.navigationBar a').removeClass('active');
+            }
+        });
+    }); //END WINDOW SCROLL LISTENER
+
+    $('.navigationBar a').on('click', function(event) {
+      $(this).parent().find('a').removeClass('active');
+      $(this).addClass('active');
     });
 
-    // $('#nav nav a').on('click', function(event) {
-    //   $(this).parent().find('a').removeClass('active');
-    //   $(this).addClass('active');
-    // });
 
-    // $(window).on('scroll', function() {
-    //     $('.target').each(function() {
-    //         if($(window).scrollTop() >= $(this).position().top) {
-    //             var id = $(this).attr('id');
-    //             $('#nav nav a').removeClass('active');
-    //             $('#nav nav a[href=#'+ id +']').addClass('active');
-    //         }
-    //     });
-    // });
+    var projectSDF = $('.projectSDF');
+    var imageSDF = $('.imageSDF');
+    //SDF PROJECT MODAL:
+    imageSDF.mouseenter(function(){
+      projectSDF.fadeIn('fast');
+    });
+    projectSDF.mouseleave(function(){
+      projectSDF.fadeOut('fast');
+    });
 
-  var projectSDF = $('.projectSDF');
-  var imageSDF = $('.imageSDF');
-  //SDF PROJECT MODAL:
-  imageSDF.mouseenter(function(){
-    projectSDF.fadeIn('fast');
-  });
-  projectSDF.mouseleave(function(){
-    projectSDF.fadeOut('fast');
-  });
+    var projectHoldem = $('.projectHoldem');
+    var imageHoldem = $('.imageHoldem');
+    //SDF PROJECT MODAL:
+    imageHoldem.mouseenter(function(){
+      projectHoldem.fadeIn('fast');
+    });
+    projectHoldem.mouseleave(function(){
+      projectHoldem.fadeOut('fast');
+    });
 
-  var projectHoldem = $('.projectHoldem');
-  var imageHoldem = $('.imageHoldem');
-  //SDF PROJECT MODAL:
-  imageHoldem.mouseenter(function(){
-    projectHoldem.fadeIn('fast');
-  });
-  projectHoldem.mouseleave(function(){
-    projectHoldem.fadeOut('fast');
-  });
+    var projectSygns = $('.projectSygns');
+    var imageSygns = $('.imageSygns');
+    //SDF PROJECT MODAL:
+    imageSygns.mouseenter(function(){
+      projectSygns.fadeIn('fast');
+    });
+    projectSygns.mouseleave(function(){
+      projectSygns.fadeOut('fast');
+    });
 
-  var projectSygns = $('.projectSygns');
-  var imageSygns = $('.imageSygns');
-  //SDF PROJECT MODAL:
-  imageSygns.mouseenter(function(){
-    projectSygns.fadeIn('fast');
-  });
-  projectSygns.mouseleave(function(){
-    projectSygns.fadeOut('fast');
-  });
-
-  var projectTeachMe = $('.projectTeachMe');
-  var imageTeachMe = $('.imageTeachMe');
-  //SDF PROJECT MODAL:
-  imageTeachMe.mouseenter(function(){
-    projectTeachMe.fadeIn('fast');
-  });
-  projectTeachMe.mouseleave(function(){
-    projectTeachMe.fadeOut('fast');
-  });
+    var projectTeachMe = $('.projectTeachMe');
+    var imageTeachMe = $('.imageTeachMe');
+    //SDF PROJECT MODAL:
+    imageTeachMe.mouseenter(function(){
+      projectTeachMe.fadeIn('fast');
+    });
+    projectTeachMe.mouseleave(function(){
+      projectTeachMe.fadeOut('fast');
+    });
 
 
 });
